9b16af5416dce10fc105e5887509e05639807382
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 13:19:13 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "e9f82e6d-d0c4-45e6-a4fb-b5605c2f39f1",
+    "home_feed": [
+        {
+            "id": "e67a3daf-f69b-422c-9c12-bdecd6bdf5eb",
+            "artist_name": "their",
+            "home_carousel": [
+                {
+                    "id": "ff694b55-b94a-464b-a377-3eb432e149d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "1ea13f48-8acb-4701-9964-3fd9b0faff03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/274.png"
+                },
+                {
+                    "id": "d836f8d2-e04a-4b61-b8d7-38754c08b619",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/126.png"
+                },
+                {
+                    "id": "4a52ce2e-12f0-4662-8cfc-bd5ebfaabef8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                },
+                {
+                    "id": "dca1a9b6-6365-49c1-aed0-952e6ba2e201",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                }
+            ],
+            "art_caption": "fear",
+            "art_description": "Activity nature the. Chair might respond few."
+        },
+        {
+            "id": "33024a84-2940-40c0-8353-0b6638e33e0d",
+            "artist_name": "play",
+            "home_carousel": [
+                {
+                    "id": "e5a2f708-af86-4446-9a74-d5ee1e8ccd22",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png"
+                },
+                {
+                    "id": "11640e88-89b3-4b86-a078-4304015fda23",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "be49ebbb-2090-498d-9e27-d5f7135e6a3c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png"
+                },
+                {
+                    "id": "d07cb316-128b-4043-9f78-1a9f9af8ff3a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "d50410c0-5116-4edb-a35b-b1dfde24882b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                }
+            ],
+            "art_caption": "because",
+            "art_description": "Consumer image of few his simply."
+        },
+        {
+            "id": "d4145444-21bc-4cd4-bc22-6c0c2e16f83b",
+            "artist_name": "along",
+            "home_carousel": [
+                {
+                    "id": "1f94d716-6e9c-4648-9b48-30d92c3f1350",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/22.png"
+                },
+                {
+                    "id": "3a9ab3ab-56fe-4edb-8aa4-2e7ffc59f637",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/286.png"
+                },
+                {
+                    "id": "9be4e5f0-ccf8-4d2b-bb80-bda3a54eaa66",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+                },
+                {
+                    "id": "d635f580-5e7e-4ffe-a6c0-0256b8fa1d2a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "33cc7365-e032-47fb-a1ed-7301697dc7d4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/122.png"
+                }
+            ],
+            "art_caption": "teacher",
+            "art_description": "Side bank dinner begin eye hospital. Condition practice information throughout end politics activity. Western age somebody business contain free staff identify."
+        },
+        {
+            "id": "1c3a584c-ff4e-49f7-b1a4-522fd243e485",
+            "artist_name": "agent",
+            "home_carousel": [
+                {
+                    "id": "4bd9d4d6-6a0f-4693-8d01-f48d17e6e859",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "d112f8ab-c65a-482c-a8e5-f7bafeefa7ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                },
+                {
+                    "id": "23659401-64a9-47d7-89a0-e903ebaa78c8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "886cc2fc-021e-4c1b-9ce4-d458582a644e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/211.png"
+                },
+                {
+                    "id": "d26dcdb2-0e16-4837-851b-0eef7c4f24f1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                }
+            ],
+            "art_caption": "glass",
+            "art_description": "Trouble example area star girl daughter matter education. Could try also as those. Seat culture reflect significant shake everything article."
+        },
+        {
+            "id": "ea70675c-1101-494c-94c3-c18bc9eee53e",
+            "artist_name": "look",
+            "home_carousel": [
+                {
+                    "id": "240d8e5d-e7de-45f1-823a-2365c21a4332",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/48.png"
+                },
+                {
+                    "id": "be5525de-66af-4d9c-abf0-6273c9396356",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/141.png"
+                },
+                {
+                    "id": "72313c32-2a3e-4ce0-91b1-50694a3ff90c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                },
+                {
+                    "id": "50b5edcd-6b5d-4979-a9e4-7ade95f3043f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "9bb1db50-a512-47dc-8e20-931ab21623dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/296.png"
+                }
+            ],
+            "art_caption": "pretty",
+            "art_description": "Likely fast style building explain far out. South point deep customer agreement recognize. Actually dream possible."
+        }
+    ]
+}
